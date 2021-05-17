559d1daf0738af451dab2186af5c7d5ba7db0d57
Mário César Silva <mcgato33@hotmail.com>
2021-05-17 06:58:00 -0100

    create Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,39 @@
+import Image from 'next/image';
+import { 
+  BellIcon,
+  ChatIcon,
+  ChevronDownIcon,
+  UserGroupIcon,
+  ViewGridIcon,
+} from '@heroicons/react/solid';
+import {
+  FlagIcon,
+  PlayIcon,
+  SearchIcon,
+  ShoppingCartIcon
+} from '@heroicons/react/outline';
+
 export function Header() {
   return (
     <div>
-      Header
+      <h1 className="text-6xl">Header</h1>
+
+      {/**Left */}
+      <div className="flex items-center">
+        <Image 
+          src="https://links.papareact.com/5me" 
+          alt="facebook logo" 
+          height={40}
+          width={40}
+          layout="fixed"
+        />
+        <div className="flex ml-2 items-center rounded-full">
+          <SearchIcon className="h-6" />
+          <input type="text" placeholder="Search Facebook" />
+        </div>
+      </div>
+      {/**Center */}
+      {/**Right */}
     </div>
   );
 }
